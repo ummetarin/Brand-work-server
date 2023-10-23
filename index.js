9db421410255b82a581b59433b844e9809206d4e
ummetarin <ummehomairatarin@gmail.com>
2023-10-23 12:33:12 +0600

    Added card delete opration

--- index.js
+++ index.js
@@ -100,6 +100,12 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/dataforcard/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await cardCollection.deleteOne(query);
+      res.send(result);
+    })
    
 
     
